ff5b238eef91f7a2c862f301d28ef204c8ec4ec8
ddl-vasu <99897438+ddl-vasu@users.noreply.github.com>
2023-07-30 23:44:34 +0530

    Update update-repo.sh

--- update-repo.sh
+++ update-repo.sh
@@ -3,7 +3,7 @@ echo "Edited by : $1" >> /mnt/code/new_file.txt
 ssh-keyscan github.com  >> ~/.ssh/known_hosts
 git config --global user.email "vasu.arora@dominodatalab.com"
 git config --global user.name "Owner"
-git remote add origin ssh://ddl-vasu@github.com:ddl-vasu/ddl-git-test.git
+git remote set-url origin ssh://ddl-vasu@github.com:ddl-vasu/ddl-git-test.git
 git add .
 git commit -m "test launcher user"
 git push
