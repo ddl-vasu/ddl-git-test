d818a4d653687ab28eedd256bc97654fd9fb546c
ddl-vasu <99897438+ddl-vasu@users.noreply.github.com>
2023-07-31 00:11:12 +0530

    Update update-repo.sh

--- update-repo.sh
+++ update-repo.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Edited by : $1" >> /mnt/code/new_file.txt
-touch ~/.ssh/known_hosts
 ssh-keyscan -t ed25519 github.com  > ~/.ssh/known_hosts
 git config --global user.email "vasu.arora@dominodatalab.com"
 git config --global user.name "Owner"
